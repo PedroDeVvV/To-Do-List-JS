1b96c2b1fcae25022f1e857b11f7ce30760d3418
PedroDeVvV <pedrohgs2004@gmail.com>
2022-11-28 08:24:28 -0300

    First Commit

---
+++ .github/README.md
@@ -0,0 +1,19 @@
+## Lista de Tarefas em JavaScript
+
+<h3>✍️Objetivo</h3>
+
+O objetivo desse projeto foi reunir o conhecimento obtido até o momento, colocando em prática funções, DOM, arrays e manipulação de eventos.
+
+![preview](./LT.gif)
+🔗[Clique aqui para acessar](https://pedrodevvv.github.io/Timer-JS/)
+## 🛠️Tecnologias
+
+* JavaScript
+* HTML
+* CSS
+* Visual Studio Code
+
+## 📞 Contato
+
+📩E-mail: pedrohgs2004@gmail.com <br>
+🌐Linkedin: https://www.linkedin.com/in/pedro-henrique-g-silva-a2100a23a/?originalSubdomain=br
---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+const inputTarefas = document.querySelector('.input-tarefas');
+const btnTarefas = document.querySelector('.btn-add');
+const tarefas = document.querySelector('.tarefas');
+
+inputTarefas.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        criaTarefa(inputTarefas.value);
+    }
+});
+
+function limpaInput() {
+    inputTarefas.value = '';
+    inputTarefas.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar')
+    li.appendChild(botaoApagar);
+}
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefas();
+}
+
+btnTarefas.addEventListener('click', function (e) {
+    if (!inputTarefas.value == "") {
+        criaTarefa(inputTarefas.value);
+    } else {
+        alert('Informe a tarefa desejada!');
+    }
+});
+
+document.addEventListener('click', function (e) {
+    const el = e.target;
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+        salvarTarefas();
+    }
+});
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim;
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+    }
+}
+
+adicionaTarefasSalvas()
